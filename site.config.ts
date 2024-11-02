dfa57df36ec011f48ca4f23b2ba7edd76116b42e
mafuyusame <mafuyu@tutamail.com>
2024-11-02 15:01:27 +0900

    루트 노션 페이지 ID 변경

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '1320bcadb10780c2a657e7bf0db69672',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
